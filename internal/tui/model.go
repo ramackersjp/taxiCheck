de0f332db843e5bf54100ead61fe1b6317fbe0bc
J.P. Ramackers <jramackers@proton.me>
2026-08-27 03:26:46 +0200

    fix: data race on appVersion - capture value before goroutine launch

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -848,6 +848,7 @@ func (m Model) updateResult(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) checkUpdate() tea.Cmd {
+	currentVer := appVersion
 	return func() tea.Msg {
 		client := &http.Client{Timeout: 10 * time.Second}
 		resp, err := client.Get("https://api.github.com/repos/jp/taxiprijs/releases/latest")
@@ -867,7 +868,7 @@ func (m Model) checkUpdate() tea.Cmd {
 			return updateCheckMsg{err: err}
 		}
 
-		hasUpdate := release.TagName != appVersion
+		hasUpdate := release.TagName != currentVer
 		return updateCheckMsg{
 			latestTag: release.TagName,
 			hasUpdate: hasUpdate,
